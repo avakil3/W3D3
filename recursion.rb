4bfbc4c2a6ac56a7f1cc2c8f4824a8107837410e
Deanding <dingtian.ding@gmail.com>
2021-12-15 15:46:12 -0500

    afternoon break

--- recursion.rb
+++ recursion.rb
@@ -36,3 +36,63 @@ class Array
          new_arr
     end
 end
+
+def fibs_recur(n)
+    return [] if n == 0
+    return [1] if n == 1
+    return [1,1] if n == 2
+
+    prev = fibs_recur(n-1)
+    prev << prev[-1] + prev[-2]
+    
+end
+
+def fibs_iter(n)
+    return [] if n == 0
+    return [1] if n == 1
+    return [1,1] if n == 2
+
+    fibs = [1,1]
+
+    while fibs.length < n
+        fibs << fibs[-1] + fibs [-2]
+    end
+    fibs
+end
+
+# p fibs_iter(8)  #1 1 2 3 5 
+
+def bsearch(array, target)
+    debugger
+    return nil if array.empty?
+
+    middle_index = array.length.even? ? array.length/2 - 1 : array.length / 2
+    middle_value = array[middle_index]
+    index = 0
+
+    case target <=> middle_value 
+    when 1 # target > middle  = to the right = add onto index
+        bsearch(array[middle_index + 1..-1], target) #2 + 1 
+        index += middle_index # = 2
+    when 0 # target = middle    
+        index += middle_index
+    when -1 #target < middle  = to the left = minus  index  
+        bsearch(array[0...middle_index], target) #2 + 1 
+        index += (middle_index - array[0...middle_index].length) # = 2
+    end
+    index
+end
+
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5 
+# 1st  index = 0, middle index = 2, set index = 2
+# 2nd  [4,5,6] index = 0(),  middle =1 , set index = 1(3?)
+# 3rd [6]   index = 0
+# p bsearch([1, 2, 3, 4, 5, 6], 1) # => 0  2 => 0
+
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil 
